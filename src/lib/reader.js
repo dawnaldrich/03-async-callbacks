54f8ec0e2cd981e1827c6b5e8b750ccbd5c4880f
dawnaldrich <dawnstarr.aldrich@gmail.com>
2018-04-28 18:54:15 -0700

    files successfully read - adding README

--- src/lib/reader.js
+++ src/lib/reader.js
@@ -1,40 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+const logger = require('./logger');
 
-const reader = module.exports = {};
+const fileReader = module.exports = {};
 
-reader.readFiles = (paths, callback) => {
-  fs.readFile(paths[0], 'utf8', (err1, data1) => {
-    if (err1) {
-      return callback(err1);
-    }
-    callback(data1);
-    return fs.readFile(paths[1], 'utf8', (err2, data2) => {
-      if (err2) {
-        return callback(err2);
+fileReader.readFirstNCharacterAsync = (filePath, characters, callback) => {
+  console.log(characters);
+  logger.log(logger.VERBOSE, `Reading ${characters}`);
+
+  return fs.readFile(
+    filePath,
+    (error, fileBuffer) => {
+      if (error) {
+        throw new Error(' coming from line 15 reader');
       }
-      callback(data2);
-      return fs.readFile(paths[2], 'utf8', (err3, data3) => {
-        if (err3) {
-          return callback(err3);
-        }
-        callback(data3);
-        return undefined;
-      });
-    });
-  });
-}
-
-// fileReader.mapFilesAsync = (paths, callback) => {
-//   const pathArray = paths;
-//   for (let i = 0; i < pathArray.length; i++) {
-//     fs.readFile(pathArray[i], (error, fileBuffer) => {
-//       if (error) {
-//         throw error;
-//       }
-//       return callback(null, fileBuffer.toString('utf8'));
-//     });
-//   }
-// };
+      return callback(fileBuffer.toString('utf8', 0, characters));
+    },
+  );
+};
 
